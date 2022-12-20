53649e641335caf49b10ca126865da989fdfebef
Milo Bonnet <milovtt@gmail.com>
2022-12-20 14:02:37 +0100

    Correction JavaScript

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-    let btn = document.getElementById('btn');
+const btn = document.getElementById('btn')
+const quote = document.getElementById('qte')
+const author = document.getElementById('author')
+const image = document.getElementById('image')
 
-    btn.addEventListener("click", function() {
-        const reqListener = response => {
-            let temp =JSON.parse(response.currentTarget.response) ;
-            
-            document.getElementById('qte').innerHTML = temp.quote
-            document.getElementById('author').textContent = temp.author
-            document.getElementById('image').src = temp.photo
-            
-          };
-    
-        
-        const loadData = () => {
-            const req = new XMLHttpRequest();
-            req.onload = reqListener;
-            req.open("get", "https://thatsthespir.it/api", true);
-            req.send();
-        };
+btn.addEventListener('click', function () {
+  const reqListener = response => {
+    const temp = JSON.parse(response.currentTarget.response)
 
+    quote.innerHTML = temp.quote
+    author.textContent = temp.author
+    image.src = temp.photo
+  }
 
-        window.onload = () => {
-            btn.onclick = loadData;
-        };
-        loadData()
+  const loadData = () => {
+    const req = new XMLHttpRequest()
+    req.onload = reqListener
+    req.open('get', 'https://thatsthespir.it/api')
+    req.send()
+  }
 
-    })
+  window.onload = () => {
+    btn.onclick = loadData
+  }
+  loadData()
+})
